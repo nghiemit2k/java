d5e4ff59c38c4e288ef4790fc3166144a69c912d
nghiem <nghiem066@gmail.com>
2022-10-23 18:29:21 +0700

    update code

--- src/test.java
+++ src/test.java
@@ -2,8 +2,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello worl");
         System.out.println("2222");
-        int a=10;
-        int b=5;
-        System.out.println(a+b);
+        int a = 10;
+        int b = 5;
+        if (a > 0) {
+            System.out.println(a + b);
+        }
     }
 }
