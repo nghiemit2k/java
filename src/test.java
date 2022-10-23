735844f16aa630d0fd870e9951f5ee955a437772
nghiem <nghiem066@gmail.com>
2022-10-23 18:07:44 +0700

    update code cho nhanh feature-test

--- src/test.java
+++ src/test.java
@@ -2,5 +2,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello worl");
         System.out.println("2222");
+        int a=10;
+        int b=5;
+        System.out.println(a+b);
     }
 }
