128b83fd816cb9b4845962d77f96bc58580f3069
nghiem <nghiem066@gmail.com>
2022-10-23 17:17:50 +0700

    sua code demo

--- src/test.java
+++ src/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello worl");
+        System.out.println("2222");
     }
 }
